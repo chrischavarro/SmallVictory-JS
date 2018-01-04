f92110059ef2007b73b26b03a9dfc61523d392d1
Christian Chavarro <christian@clutchprep.com>
2018-01-03 23:05:33 -0600

    One line shift later...user track push issue fixed :)

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -12,18 +12,19 @@ class Dashboard extends Component {
     if (this.props.summary) {
       const completed = this.props.summary[0].completed.length
       const attempted = this.props.summary[1].attempted.length
-      const percentage = this.props.summary[2]
+      const percentage = this.props.summary[3]
+      const track = this.props.summary[2][0]
       console.log(this.props.summary)
       // console.log(this.props.summary)
-
+// EXTRAPOLATE THIS INTO ITS OWN COMPONENT
       return (
         <div className="row">
         <div className="col s6 offset-s3 center-align" style={{ }}>
-        <h4>Whats up, {name}!</h4>
+        <h4>Whats up, {name}?</h4>
         <h4>Youre currently on a x day streak</h4>
         <h4>Youve completed {completed} out of {attempted} victories</h4>
         <h4>Youve completed {percentage}% of your victories!</h4>
-        <h4>Youre on the x track</h4>
+        <h4>Youre on the {track} track</h4>
         <h4>Ready for todays task?</h4>
         </div>
         </div>
--- routes/summaryController.js
+++ routes/summaryController.js
@@ -10,46 +10,38 @@ summaryController.get('/api/summary', (req, res) => {
   const attempted = [];
   const completed = [];
   const summary = [];
-  var percentage = 0
   const trackList = [];
 
-  // Completion.find({ user_id: userId })
-  //   .exec((err, completions) => {
-  //     console.log('Completions found', completions.length)
-  //     if (completions.length > 0) {
-  //       var percentage = (completed.length/attempted.length) * 100;
-  //       completions.forEach((completion) => {
-  //         if (completion.completed) {
-  //           completed.push(completion)
-  //         }
-  //         attempted.push(attempted)
-  //       })
-  //     }
-  //     // else {
-  //     //   console.log('Zero completions')
-  //     // }
-  //
-  //   })
-    Profile.findById(profile)
-    .populate('tracks')
-    .exec((err, profile) => {
+  Completion.find({ user_id: userId })
+    .exec((err, completions) => {
+      // console.log('Completions found', completions.length)
+      if (completions.length >= 1) {
+        var percentage = (completed.length/attempted.length) * 100;
+        completions.forEach((completion) => {
+          if (completion.completed) {
+            completed.push(completion)
+          }
+          attempted.push(attempted)
+        })
+      }
+      else {
+        var percentage = 0;
+      }
+      Profile.findById(profile)
+      .populate('tracks')
+      .exec((err, profile) => {
+        profile.tracks.forEach((track) => {
+          trackList.push(track.name)
+        })
+        summary.push({'completed': completed})
+        summary.push({'attempted': attempted})
+        summary.push(trackList)
+        summary.push(percentage)
 
-      async.each(profile.tracks, (track, cb) => {
-        trackList.push(track.name)
-        console.log(track.name)
+        res.send(summary)
       })
 
-      // profile.tracks.forEach((track) => {
-      //   console.log('TRACK LIST', track.name)
-      // })
     })
-  summary.push({'completed': completed})
-  summary.push({'attempted': attempted})
-  summary.push(trackList)
-  summary.push(percentage)
-  console.log('TRACK ARRAY', trackList)
-
-  res.send(summary)
 })
 
 module.exports = summaryController;
