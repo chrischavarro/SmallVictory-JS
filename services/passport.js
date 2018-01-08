3781bb05fe91f1788be2b80c4c40206ef924f8c4
Christian Chavarro <christian@clutchprep.com>
2018-01-08 13:54:44 -0600

    Typo fix 3

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-const User = require('../models/user');
+const User = require('../models/User');
 
 passport.serializeUser((user, cb) => {
   cb(null, user._id);
