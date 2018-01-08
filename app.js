a6827149b412603e9e98659e295b525b7e5a792a
Christian Chavarro <christian@clutchprep.com>
2018-01-08 13:48:18 -0600

    Deployment fix attempt 1

--- app.js
+++ app.js
@@ -78,6 +78,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port);
+app.listen(port, function() {
+  console.log('Small victory server running on port ' + port)
+});
 
 module.exports = app;
