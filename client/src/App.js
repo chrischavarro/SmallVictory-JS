55bd32b7f4ee14d26f8ffab6cd1ba03277ece0f1
Christian Chavarro <christian@clutchprep.com>
2018-01-10 22:38:28 -0600

    Changed page transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AnimatedSwitch, AnimatedRoute, spring } from 'react-router-transition';
-// import spring from '../../src/spring';
+// import spring from 'react-motion';
 import { connect } from 'react-redux';
 import Home from './components/Home/Home';
 import Header from './components/Header';
@@ -26,12 +26,24 @@ function slide(val) {
   });
 }
 
+// const pageTransitions = {
+//   atEnter: {
+//     opacity: 0
+//   },
+//   atLeave: {
+//     opacity: 0
+//   },
+//   atActive: {
+//     opacity: 1
+//   },
+// };
+
 const pageTransitions = {
   atEnter: {
-    offset: 100,
+    offset: 100
   },
   atLeave: {
-    offset: glide(-100),
+    offset: glide(100),
   },
   atActive: {
     offset: glide(0),
@@ -50,35 +62,79 @@ const topBarTransitions = {
   },
 };
 
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22,
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1),
+  },
+};
 class App extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
 
+
+  // <AnimatedSwitch
+  // className="switch-wrapper"
+  // {...pageTransitions}
+  // mapStyles={styles => ({
+  //   transform: `translateX(${styles.offset}%)`,
+  // })}
+  // >
   render() {
     return (
       <div>
         <BrowserRouter>
           <div>
             <Header />
-            <AnimatedSwitch
-              className="switch-wrapper"
-              {...pageTransitions}
-              mapStyles={styles => ({
-                transform: `translateX(${styles.offset}%)`,
-              })}
-            >
-              <Route exact path="/" component={Home} />
-              <Route exact path="/dashboard" component={Dashboard} key="dashboard-key" />
-              <Route exact path="/tasks/new" component={TaskNew} />
+              <AnimatedRoute
+                exact path="/"
+                component={Home}
+                {...pageTransitions}
+              />
+              <AnimatedRoute
+                exact path="/dashboard"
+                component={Dashboard}
+                atEnter={bounceTransition.atEnter}
+atLeave={bounceTransition.atLeave}
+atActive={bounceTransition.atActive}
+
+              />
+              <AnimatedRoute
+                exact path="/tasks/new"
+                component={TaskNew}
+                atEnter={bounceTransition.atEnter}
+atLeave={bounceTransition.atLeave}
+atActive={bounceTransition.atActive}
+              />
               <Route path="/select-track" component={SelectTrack} />
-            </AnimatedSwitch>
           </div>
         </BrowserRouter>
       </div>
     );
   }
 }
+// </AnimatedSwitch>
 
 
 export default connect(null, actions)(App);
